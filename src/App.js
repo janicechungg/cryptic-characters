30e786a9834b7a4e84d643db64debebb8dd64d8a
Janice Chung <142273559+janicechungg@users.noreply.github.com>
2024-06-10 16:25:42 -0500

    added english words txt file and created header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import GameName from './Components/GameName';
 
 function App() {
   return (
     <div className="App">
-      
+      <GameName/>
     </div>
   );
 }
+
+
 
 export default App;
---
+++ src/Components/GameName.js
@@ -0,0 +1,9 @@
+const GameName = () => {
+  return (
+    <div>
+      <h1>Cryptic Characters</h1>
+    </div>
+  )
+}
+
+export default GameName;
